392a1189b0fe8e9e0862066e44b612a15d924656
exoldarium <shandoo91@gmail.com>
2023-11-28 23:14:08 +0700

    refactored exports

--- utils/parsingHelpers.ts
+++ utils/parsingHelpers.ts
@@ -1,46 +1,46 @@
 
-const isString = (string: unknown): string is string => {
+export const isString = (string: unknown): string is string => {
   return typeof string === 'string' || string instanceof String;
 };
 
-const isNumber = (number: unknown): number is number => {
+export const isNumber = (number: unknown): number is number => {
   return typeof number === 'number' || number instanceof Number;
 };
 
-const isBool = (bool: unknown): bool is boolean => {
+export const isBool = (bool: unknown): bool is boolean => {
   return typeof bool === 'boolean' || bool instanceof Boolean;
 };
 
-const isDate = (date: unknown): boolean => {
+export const isDate = (date: unknown): boolean => {
   if (!isString(date)) throw new Error('Invalid date input');
   return Boolean(Date.parse(date));
 };
 
-const parseToString = (param: unknown): string => {
+export const parseToString = (param: unknown): string => {
   if (!isString(param)) throw new Error('Invalid string input');
 
   return param;
 };
 
-const parseToBool = (param: unknown): boolean => {
+export const parseToBool = (param: unknown): boolean => {
   if (!isBool(param)) throw new Error('Invalid boolean input');
 
   return param;
 };
 
-const parseDate = (param: unknown): string => {
+export const parseDate = (param: unknown): string => {
   if (!isString(param) || !isDate(param)) throw new Error('Invalid date input');
 
   return param;
 };
 
-const parseToNumber = (param: unknown): number => {
+export const parseToNumber = (param: unknown): number => {
   if (!isNumber(param)) throw new Error('Invalid number input');
 
   return param;
 };
 
-const parseError = (error: unknown) => {
+export const parseError = (error: unknown) => {
   let errorMessage = 'There was an error.';
 
   if (error instanceof Error) {
@@ -49,16 +49,4 @@ const parseError = (error: unknown) => {
 
   console.log(error);
   return errorMessage;
-};
-
-export {
-  isString,
-  isNumber,
-  isBool,
-  isDate,
-  parseToString,
-  parseDate,
-  parseToBool,
-  parseToNumber,
-  parseError
 };
